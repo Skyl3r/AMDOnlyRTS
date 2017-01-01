98fc1d035ed59e7ccd2404d354771c21809bde69
James Davis <jamesdavis0901@gmail.com>
2017-01-01 03:36:22 -0500

    resolved conflicts

---
+++ AMDOnlyRTS/AMDOnlyRTS.Contracts.Data/Interfaces/Networking/ILobby.cs
@@ -0,0 +1,16 @@
+﻿using AMDOnlyRTS.Contracts.Data.Interfaces.GameEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMDOnlyRTS.Contracts.Data.Interfaces.Networking
+{
+    public interface ILobby
+    {
+        List<IPlayerController> Players { get; set; }
+        int MaxPlayers { get; set; }
+        string MapName { get; set; }
+    }
+}
