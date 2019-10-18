2f060e80aa833e3f82763a108fd0634d88ef48ec
Skyl3r <kpereksta@gmail.com>
2019-10-18 11:51:08 -0400

    Added renderer and graphics interface. Made test renderer with Love

--- Core/Program.cs
+++ Core/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using AmdOnlyRts.Renderer.Classes;
+using Love;
 
 namespace AmdOnlyRts.Core
 {
-    class Program
+    class Program: Scene
     {
+
+        static LoveRenderer loveRenderer;
+        
+        //Example using Love Renderer
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            loveRenderer = new LoveRenderer();
+            loveRenderer.OnLoad += new OnLoad(OnRendererLoad);
+            loveRenderer.OnDraw += new OnDraw(OnRendererDraw);
+            loveRenderer.OnUpdate += new OnUpdate(OnRendererUpdate);
+            loveRenderer.Start();
+
+        }
+
+        public static void OnRendererLoad() {
+
+        }
+
+        public static void OnRendererDraw() {
+            loveRenderer.graphics.DrawText("Hello, World", 300, 400);
+        }
+
+        public static void OnRendererUpdate() {
+
         }
     }
 }
