60296c0b6ba2cb28227d92beba0efe0b89353f74
DigMan <diggly123456@gmail.com>
2019-10-17 21:50:33 -0400

    adding test stub

--- BrokenDigSky.AmdOnlyRts.Networking/Program.cs
+++ BrokenDigSky.AmdOnlyRts.Networking/Program.cs
@@ -1,5 +1,9 @@
 using BrokenDigSky.AmdOnlyRts.Domain.Interfaces.Networking;
+using BrokenDigSky.AmdOnlyRts.Networking.Client;
+using BrokenDigSky.AmdOnlyRts.Networking.Server;
 using System;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace BrokenDigSky.AmdOnlyRts.Networking
 {
@@ -7,7 +11,23 @@ namespace BrokenDigSky.AmdOnlyRts.Networking
     {
         public static void Main()
         {
-          Console.WriteLine("sup fool");
+          new Program().Run().Wait();
         }
+
+        public async Task Run()
+        {
+          // var server = new GameServer(25565);
+          // await server.Start();
+
+          var client = new GameClient("192.168.0.2", 8000, (x,y)=> Console.WriteLine($"{x}: {y}"));
+          await client.Start();
+          while(true)
+          {
+            await client.Send("Me Bitch", Console.ReadLine());
+          }
+
+
+        }
+
     }
 }
