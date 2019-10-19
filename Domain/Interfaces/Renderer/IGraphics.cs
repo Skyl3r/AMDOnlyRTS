f96721b3eab66be8868cbf89504b02dccb3bec30
Blake Harrington <blake.harrington14@gmail.com>
2019-10-19 16:08:21 -0400

    added drawrect function to igraphics

--- Domain/Interfaces/Renderer/IGraphics.cs
+++ Domain/Interfaces/Renderer/IGraphics.cs
@@ -5,5 +5,6 @@ namespace AmdOnlyRts.Domain.Interfaces.Renderer
   public interface IGraphics {
 
     void DrawText(string text, int x, int y);
+    void DrawRect(float width, float height, float x, float y);
   }
 }
