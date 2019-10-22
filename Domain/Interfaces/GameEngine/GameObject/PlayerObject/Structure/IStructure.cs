8f1cde7352780ed253f4beedf6edf6a9633398e8
Skyl3r <kpereksta@gmail.com>
2019-10-22 13:54:15 -0400

    Added PlayerObject

--- Domain/Interfaces/GameEngine/GameObject/PlayerObject/Structure/IStructure.cs
+++ Domain/Interfaces/GameEngine/GameObject/PlayerObject/Structure/IStructure.cs
@@ -1,8 +1,9 @@
-﻿
+﻿using AmdOnlyRts.Domain.Interfaces.GameEngine.Game;
 
 namespace AmdOnlyRts.Domain.Interfaces.GameEngine.GameObject.PlayerObject.Structure
 {
 	public interface IStructure : IPlayerObject
 	{
+		IPlayer Owner { get; set; }
 	}
 }
