7e597163eea6cf8e8bd9b4cc1ce222e77e2ec541
Skyl3r <kpereksta@gmail.com>
2019-10-22 13:55:20 -0400

    Fixed typo

--- Domain/Interfaces/GameEngine/GameObject/PlayerObject/PlayerObject.cs
+++ Domain/Interfaces/GameEngine/GameObject/PlayerObject/PlayerObject.cs
@@ -1,9 +1,11 @@
 
+using AmdOnlyRts.Domain.Interfaces.GameEngine.Game;
+
 namespace AmdOnlyRts.Domain.Interfaces.GameEngine.GameObject.PlayerObject
 {
 	public interface IPlayerObject: IGameObject
 	{
-
+		IPlayer Owner { get; set; }
 
 	}
 }
